14636d68ad97e17a2cec464e98256986f146eda8
nguyenduyphuc <duyphucnguyen95@gmail.com>
2017-11-06 10:48:02 +0700

    first commit

---
+++ resources/views/widgets/footer_tintuc.blade.php
@@ -0,0 +1,33 @@
+
+
+
+
+
+<footer id="footer">    
+    <div class="footer_bottom">
+     
+      <p class="developer">
+      Trường ĐHCNTT Thành Phố Hồ Chí Minh</br> 
+      &copy; Copyright Duy Phúc <a href="index.html">2017</a></p>
+    </div>
+  </footer>
+
+
+
+
+
+
+
+
+
+  
+<script src="{{url('/')}}/assets/js/jquery.min.js"></script> 
+<script src="{{url('/')}}/assets/js/wow.min.js"></script> 
+<script src="{{url('/')}}/assets/js/bootstrap.min.js"></script> 
+<script src="{{url('/')}}/assets/js/slick.min.js"></script> 
+<script src="{{url('/')}}/assets/js/jquery.li-scroller.1.0.js"></script> 
+<script src="{{url('/')}}/assets/js/jquery.newsTicker.min.js"></script> 
+<script src="{{url('/')}}/assets/js/jquery.fancybox.pack.js"></script> 
+<script src="{{url('/')}}/assets/js/custom.js"></script>
+</body>
+</html>
